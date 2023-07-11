a408540fa824c80420b36e0e8aec990aba1d7b34
unknown <sjunior863@gmail.com>
2023-07-10 21:55:21 -0300

    first commit

---
+++ src/main/java/com/intensivoJava/dslist/DslistApplication.java
@@ -0,0 +1,13 @@
+package com.intensivoJava.dslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistApplication.class, args);
+	}
+
+}
