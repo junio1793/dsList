1f39e45351fda15cca4c6c707802216f08e0ed0d
unknown <sjunior863@gmail.com>
2023-07-11 22:27:26 -0300

    service att

--- src/main/java/com/intensivoJava/dslist/gameService/GameService.java
+++ src/main/java/com/intensivoJava/dslist/gameService/GameService.java
@@ -1,9 +1,13 @@
 package com.intensivoJava.dslist.gameService;
 
+import com.intensivoJava.dslist.DTO.GameMinDTO;
+import com.intensivoJava.dslist.DTO.GaneDTO;
 import com.intensivoJava.dslist.entities.Game;
 import com.intensivoJava.dslist.repository.Repositorio;
+import org.apache.logging.log4j.util.ReadOnlyStringMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,8 +17,14 @@ public class GameService {
     @Autowired
     Repositorio repository;
 
-    public List<Game> findAll(){
+    @Transactional(readOnly = true)
+    public GaneDTO findById(Long id){
+        Game result = repository.findById(id).get();
+        return new GaneDTO(result);
+    }
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findAll(){
         List<Game> game = repository.findAll();
-        return game;
+        return game.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
